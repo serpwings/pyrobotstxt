af7917349907726e2d9d54291742cc81679dcc37
Seo Wings <seowingsorg@gmail.com>
2022-12-04 14:27:37 +0100

    Fix for #3
    Included Scripts Working Example  in example folder

---
+++ examples/complete_example.py
@@ -0,0 +1,37 @@
+from pyrobotstxt import RobotsTxt, UserAgent
+
+robots_file = RobotsTxt()
+
+robots_file.include_header("Welcome Crawlers", append_date=True)
+robots_file.include_footer("Good Bye Crawlers")
+
+ua_general = UserAgent(ua_name="*")
+ua_general.add_allow(
+    allow_items=["/home", "/deep", "/home"],
+    unique=True,
+    comments="This is a list of allowed items",
+)
+
+ua_general.add_disallow(
+    disallow_items=["/nopi$", "/topi?a", "/img*.png$"],
+    unique=True,
+    comments="This is a list of allowed items",
+)
+
+ua_general_google = UserAgent(ua_name="Google")
+ua_general_google.add_allow(
+    allow_items=["/home", "/deep", "/home"],
+    unique=True,
+    comments="This is a list of allowed items",
+)
+ua_general_google.add_disallow(
+    disallow_items=["/nopi$", "/topi?a", "/img*.png$"],
+    unique=True,
+    comments="This is a list of allowed items",
+)
+ua_general_google.add_sitemap("https://seowings.org/sitemap.xml")
+
+robots_file.add_user_agent(ua_general)
+robots_file.add_user_agent(ua_general_google)
+
+robots_file.write("robots.txt")
